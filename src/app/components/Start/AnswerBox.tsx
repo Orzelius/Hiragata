e019f84496e82a8c73bd9520a297680e7725bd50
Orzelius <albertkostusev@gmail.com>
2020-01-19 21:43:08 +0200

    finish on question box

--- src/app/components/Start/AnswerBox.tsx
+++ src/app/components/Start/AnswerBox.tsx
@@ -6,9 +6,16 @@ interface answerProps {
   onClick(): void
 }
 
-const AnswerBox:React.FC<answerProps> = ({title, text, onClick}) => {
-  return(
-    <div className="w-48 h-48 border-gray-300 rounded-xl border-4 m-2">
+const AnswerBox: React.FC<answerProps> = ({ title, text, onClick }) => {
+  return (
+    <div
+      className="border-gray-600 border-4 rounded-lg p-1 mt-4 hover:bg-gray-200 cursor-pointer sm:w-48 sm:h-48 sm:mx-2">
+      <div className="inline-block w-32 sm:block sm:w-auto">
+        <h1 className="text-center text-gray-900 text-4xl px-4 sm:text-center sm:p-1 sm:m-1 sm:font-bold sm:text-6xl sm:text-gray-700">{title}</h1>
+      </div>
+      <div style={{top: "-5px"}} className=" relative inline-block content-center pb-2 sm:block sm:mt-4">
+        <h1 className="text-2xl text-gray-800 sm:text-center ml-2 sm:ml-0">{text}</h1>
+      </div>
     </div>
   )
 }
--- src/app/components/Start/QueryDashboard.tsx
+++ src/app/components/Start/QueryDashboard.tsx
@@ -1,20 +1,18 @@
 import * as React from 'react';
 import { Button, colors, Container, Title } from '../BasicComponents';
 import AnswerBox from './AnswerBox';
-import Example from './example';
 
 const QueryDashboard: React.FC = () => {
   return (
     <Container>
-      <Example></Example>
-      {/* <div className="flex justify-center mt-20">
+      <div className="flex justify-center mt-20">
         <Title>A quick setup before you start</Title>
       </div>
-      <div className="flex justify-center mt-8">
-        <AnswerBox onClick={() => {}} text="" title=""></AnswerBox>
-        <AnswerBox onClick={() => {}} text="" title=""></AnswerBox>
-        <AnswerBox onClick={() => {}} text="" title=""></AnswerBox>
-      </div> */}
+      <div className="justify-center mt-8 sm:flex">
+        <AnswerBox onClick={() => { }} text="hiragana" title="か"></AnswerBox>
+        <AnswerBox onClick={() => { }} text="katakana" title="カ"></AnswerBox>
+        <AnswerBox onClick={() => { }} text="both" title="カか"></AnswerBox>
+      </div>
     </Container>
   );
 }
