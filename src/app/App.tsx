afc48fd82f1f5bc431fb6705cd6d9d913ac2e7b9
Orzelius <albertkostusev@gmail.com>
2020-07-05 11:28:57 +0300

    Remove legacy code and add pictures of mnemonics Also changed file structure a little

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,5 @@
 import * as React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import HomeOld from './components/Legacy/Start/Home_Old';
-import LearnOld from './components/Legacy/Learn/Learn';
-import StartOld from './components/Legacy/Start/Start';
 import Home from './components/Home/Home';
 import Navbar from './components/nav/Navbar';
 
@@ -14,9 +11,6 @@ const App: React.FC = () => (
       <Switch>
         {/* <Route exact path="/" component={Dasboard}/> */}
         <Route exact path="/" component={Home} />
-        <Route exact path="/o/" component={HomeOld} />
-        <Route exact path="/o/Start" component={StartOld} />
-        <Route exact path="/o/Learn" component={LearnOld} />
       </Switch>
     </div>
   </BrowserRouter>
