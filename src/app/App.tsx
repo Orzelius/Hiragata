459ff47414212848af1773d991f6c28dc23a0210
Orzelius <albertkostusev@gmail.com>
2020-07-23 13:32:47 +0300

    Update /Results

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -9,17 +9,17 @@ import Results from './components/game/results/Results';
 
 const initState: State = {
   // for more convenient testing
-  elements: [
-    { hiragana: 'や', katakana: 'ヤ', latin: 'ya' },
-    { hiragana: 'ゆ', katakana: 'ユ', latin: 'yu' },
-    { hiragana: 'よ', katakana: 'ヨ', latin: 'yo' },
-    { hiragana: 'ら', katakana: 'ラ', latin: 'ra' },
-    { hiragana: 'り', katakana: 'リ', latin: 'ri' },
-    { hiragana: 'る', katakana: 'ル', latin: 'ru' },
-    { hiragana: 'れ', katakana: 'レ', latin: 're' },
-    { hiragana: 'ろ', katakana: 'ロ', latin: 'ro' },
-  ],
-  // elements: null,
+  // elements: [
+  //   { hiragana: 'や', katakana: 'ヤ', latin: 'ya' },
+  //   { hiragana: 'ゆ', katakana: 'ユ', latin: 'yu' },
+  //   { hiragana: 'よ', katakana: 'ヨ', latin: 'yo' },
+  //   { hiragana: 'ら', katakana: 'ラ', latin: 'ra' },
+  //   { hiragana: 'り', katakana: 'リ', latin: 'ri' },
+  //   { hiragana: 'る', katakana: 'ル', latin: 'ru' },
+  //   { hiragana: 'れ', katakana: 'レ', latin: 're' },
+  //   { hiragana: 'ろ', katakana: 'ロ', latin: 'ro' },
+  // ],
+  elements: null,
   learningHiragana: true,
 };
 
