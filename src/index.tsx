d406b4cab123b17f41356d0c976ccd1d6588090f
Orzelius <albertkostusev@gmail.com>
2021-04-18 16:12:45 +0300

    Add pwa support

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import './style/style.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+serviceWorkerRegistration.register();
